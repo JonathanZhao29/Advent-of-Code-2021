502d0ecc43ea4ce2713e193077fefc1e4a7c343f
JonnyOnPC <38818412+JonnyOnPC@users.noreply.github.com>
2021-12-11 19:20:05 -0500

    Day 1 Part 2 Finished

--- day1_part1.py
+++ day1_part1.py
@@ -2,11 +2,11 @@
 
 #Function that returns 1 for increase, 0 for decrease
 def depthIncr(depth1, depth2):
-    if depth1 ==0:
+    if depth1 ==0: #N/A measurement on first depth value
         return 0
-    elif depth1 < depth2:
+    elif depth1 < depth2: #If there is an increase
         return 1
-    else:
+    else: #No increase
         return 0
 
 #Read input file
@@ -15,10 +15,10 @@ with open('day1_input.txt') as file:
 
 #Loop through input and count increases
 increases = 0
-depth1 = 0
-for line in contents:
-    depth2 = int(line)
-    increases += depthIncr(depth1, depth2)
+depth1 = 0 #Initial depth
+for line in contents:  #For each depth value in input
+    depth2 = int(line) 
+    increases += depthIncr(depth1, depth2) #Compare the current depth value with the previous value
     depth1 = depth2
 
 #Total increases:
---
+++ day1_part2.py
@@ -0,0 +1,27 @@
+#Jonathan Zhao's Code
+
+#Function which compares two depth windows, which returns 1 for increase, 0 for decrease
+def depthSumIncr(depth1, depth2):
+    if depth1 ==0: #N/A measurement on first depth window
+        return 0
+    elif depth1 < depth2: #If there is an increase
+        return 1
+    else: #No increase
+        return 0
+
+#Read input file
+with open('day1_input.txt') as file:
+    contents = file.readlines()
+
+#Loop through input and count increases
+increases = 0
+depth1 = int(contents[0])+int(contents[1])+int(contents[2]) #Initial depth window
+index = 1
+while index+2<len(contents): #While there are enough values for the sliding window
+    depth2 = int(contents[index])+int(contents[index+1])+int(contents[index+2]) 
+    increases += depthSumIncr(depth1, depth2) #Compare the current depth value window with the previous window
+    depth1 = depth2
+    index+=1
+
+#Total increases:
+print(increases)
