47513aadd3aff7dd40851e465e17b0b9a6865420
JonnyOnPC <38818412+JonnyOnPC@users.noreply.github.com>
2021-12-15 13:08:47 -0500

    Day 5 Part 1 Finished

---
+++ Day 5/day5_part1.py
@@ -0,0 +1,61 @@
+#Jonathan Zhao
+import numpy as np
+
+with open('Day 5\day5_input.txt') as file:
+    contents = file.readlines()
+for value in range(len(contents)): #Strips the extra space after each value
+    contents[value] = contents[value].strip()
+
+#Instructions
+#Hydrothermal vents in lines, x1,y1 -> x2,y2
+#Only horizontal and vertical lines
+#Determine the number of points where at least two lines overlap
+
+#Function to calculate each point in a line and returns an array of those points
+def calcLine(line):
+    #Format the line
+    x1 = int(line[:line.find(',')])
+    y1 = int(line[line.find(',')+1:line.find(' ')])
+    line = line[line.find('>')+1:]
+    x2 = int(line[:line.find(',')])
+    y2 = int(line[line.find(',')+1:])
+
+    points = [] # x,y
+    if x1 == x2: #Vertical Line
+        if y1 < y2:
+            for segment in range(y2-y1+1): #For each point, append it to the list of points
+                point = str(x1)+','+str((y1+segment)*10)
+                points.append(point)
+        else:
+            for segment in range(y1-y2+1): #For each point, append it to the list of points
+                point = str(x1)+','+str((y2+segment)*10)
+                points.append(point)
+    elif y1 == y2: #Horizontal Line
+        if x1 < x2: 
+            for segment in range(x2-x1+1): #For each point, append it to the list of points
+                point = str(x1+segment)+','+str(y1*10)
+                points.append(point)
+        else:
+            for segment in range(x1-x2+1): #For each point, append it to the list of points
+                point = str(x2+segment)+','+str(y1*10)
+                points.append(point)
+    return points
+
+#Create a dictionary, then calculate where each line is and add each point in the line to the dict to count overlaps.
+lines = {}
+for line in contents:
+    points = calcLine(line)
+    for point in points:
+        if point in lines: #If the point is already in the dict, increase the overlap value by 1
+            lines[point] +=1
+        else: #Add the point to the dict, and set its overlap value to 1
+            lines[point] = 1
+
+#Go through the dictionary and count every point where the overlap value was 2 or larger.
+overlap = 0
+for line in lines:
+    if lines[line] >=2:
+        overlap+=1
+
+#Print answer
+print('overlaps:',overlap)
